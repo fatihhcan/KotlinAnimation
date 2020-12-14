34ecb28d6369aff47477fc30ad472407a2121461
Fatih Can <ybsfatihcan@gmail.com>
2020-12-14 18:15:41 +0300

    anim added

--- app/src/main/java/com/example/animation/MainActivity.kt
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.animation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonAnim = AnimationUtils.loadAnimation(this,R.anim.button)
+        val oneAnim = AnimationUtils.loadAnimation(this,R.anim.oneanim)
+        val twoAnim = AnimationUtils.loadAnimation(this,R.anim.twoanim)
+        val threeAnim = AnimationUtils.loadAnimation(this,R.anim.threeanim)
+        val fourAnim = AnimationUtils.loadAnimation(this,R.anim.fouranim)
+
+        val emailText = findViewById(R.id.editTextEmailAddress) as EditText
+        val passwordText = findViewById(R.id.editTextPassword) as EditText
+        val button = findViewById(R.id.button) as Button
+
+
 
         val welcomeText = findViewById(R.id.textView) as TextView
+        val imageLogo = findViewById(R.id.imageView) as ImageView
 
-        welcomeText.startAnimation(buttonAnim)
+        welcomeText.startAnimation(oneAnim)
+        imageLogo.startAnimation(twoAnim)
+        emailText.startAnimation(threeAnim)
+        passwordText.startAnimation(threeAnim)
+        button.startAnimation(fourAnim)
+
     }
 }
