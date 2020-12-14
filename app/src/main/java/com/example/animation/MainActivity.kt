f1f18a857f7ca18b278054808115933780f468ae
Fatih Can <ybsfatihcan@gmail.com>
2020-12-14 16:57:45 +0300

    text anim added

--- app/src/main/java/com/example/animation/MainActivity.kt
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.animation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonAnim = AnimationUtils.loadAnimation(this,R.anim.button)
+
+        val welcomeText = findViewById(R.id.textView) as TextView
+
+        welcomeText.startAnimation(buttonAnim)
     }
 }
